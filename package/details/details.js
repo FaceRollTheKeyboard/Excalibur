93a4e44d2b54cee749ca11052bdca9c80d8dd318
mooshroom <mooshroom@tansuyun.cn>
2016-12-20 16:37:54 +0800

    [+]详情完成，组件编写就绪

--- package/details/details.js
+++ package/details/details.js
@@ -1,39 +1,51 @@
 /*
-组件详情 内在灵魂，沉稳坚毅
+ 组件详情 内在灵魂，沉稳坚毅
  生成时间：Sat Dec 17 2016   破门狂人R2-D2为您服务！
-*/
+ */
 define([
     'avalon',
     'text!./details.html',
-    'css!./details.css'
-], function (avalon, html, css) {
-    var vm=avalon.define({
-        $id:"details",
+    'css!./details.css',
+    '../../src/js/markdown.js',
+    '../../src/js/prettify.js',
+], function (avalon, html, css,marked,prettify) {
+    var vm = avalon.define({
+        $id: "details",
         ready: function (i) {
-            var obj=''
-            if(obj!=""){
-                require(['../../obj/Management/'+obj+'.js'], function () {
-                    start()
-                })
-            }else{
-                start()
-            }
-
-            function start(){
-                vm.reset()
-                index.html=html
-
-                //以及其他方法
-            }
 
 
+            vm.reset(i)
+            index.html = html
+
+            require(['./package/list/list.js'], function (obj) {
+              vm.list=obj.list
+            })
+
+            vm.getDemo(i)
+            vm.getDoc(i)
         },
-        reset: function () {
-            avalon.mix(vm,{
+        list:[],
+        reset: function (i) {
+            avalon.mix(vm, {
                 //要重置的东西最后都放回到这里
+                demo:'',
+                doc:'',
             })
         },
 
+        demo:'',
+        getDemo: function (i) {
+            require(['text!./lib/'+i+'/demo.html'], function (that) {
+                vm.demo=that
+            })
+        },
+        doc:'',
+        getDoc: function (i) {
+            require(['text!./lib'+i+'/readme.md'], function (that) {
+                vm.doc=marked(that)
+            })
+        }
+
     })
-    return window[vm.$id]=vm
+    return window[vm.$id] = vm
 })
